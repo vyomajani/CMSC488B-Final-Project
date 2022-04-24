332099a6d96f502a197f3fea23858fd7dea4cd8a
vyomajani <vyomajani@gmail.com>
2022-04-24 09:32:07 -0400

    Adding some ideas for the solver

--- final-proj/src/Solver.hs
+++ final-proj/src/Solver.hs
@@ -38,6 +38,9 @@ data Value = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine
 
 type Board = Map Loc Value
 
+-- Original input board (do not modify Zeros)
+type Input = Map Loc Value 
+
 -- Constants 
 
 height, width :: Int 
@@ -65,6 +68,14 @@ boxes = [(x, y) | x <- [0..2], y <- [0..2]] ++
         [(x, y) | x <- [6..8], y <- [3..5]] ++
         [(x, y) | x <- [6..8], y <- [6..8]]
 
+firstRow :: [Loc] 
+firstRow = [(0, y) | y <- [0..height - 1]]
+
+topLeft :: [Loc]
+topLeft = [(x, y) | x <- [0..2], y <- [0..2]]
+
+midLeft :: [Loc]
+
 -- should this be a parser? How are we creating sudoku boards? 
 -- Creates empty sudoku board (All 0s)
 initSudoku :: Board 
@@ -77,6 +88,24 @@ initSudoku = helper locations initBoard
 -- Functions 
 
 solve :: Board -> Board 
-solve board = 
-    helper board loc 
+solve input board = 
+    helper input board locations -- locations :: [Loc]
     where 
+        helper input board [] = board 
+        helper input board (h : t) = 
+            case lookup h board of 
+                Nothing -> error "Should not happen, always initialize sudoku in initSudoku?"
+                -- If the board has Zero, then no value has been tried yet
+                Just Zero -> tryValue Zero 
+                -- If the board has a value, but it originally had Zero there, double check that the value still works 
+                Just value -> if lookup h input == Just Zero then tryValue value 
+                            else helper board t  
+        
+        -- This function goes through every possible number that could go in the spot
+        -- If a number can go, it tries that
+        -- Otherwise, it calls helper again on a previous location to retry the values? Or changes a previous location 
+        tryValue board Zero = tryValue board One 
+        tryValue board value = case value of 
+            One -> (fold (\loc -> \acc -> acc && lookup loc board != One) True topLeft) 
+                    && (fold (\loc -> \acc -> acc && lookup loc board != One) True firstRow) 
+            Two -> 
