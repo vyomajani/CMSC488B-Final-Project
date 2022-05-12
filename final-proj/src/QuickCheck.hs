96817064288ab162ca677a42f9918b9d3c8f82e1
Samuel Howard <samuelhowardstudent@gmail.com>
2022-05-12 17:10:31 -0400

    Fixes props

--- final-proj/src/QuickCheck.hs
+++ final-proj/src/QuickCheck.hs
@@ -1,9 +1,8 @@
 module QuickCheck where 
-
-    import Control.Monad (liftM2, liftM3)
-    import Data.List (nub)
-    import Data.Map (Map)
-    import qualified Data.Map as Map
+import Control.Monad (liftM2, liftM3)
+import Data.List (nub)
+import Data.Map (Map)
+import qualified Data.Map as Map
     -- import Test.QuickCheck
     --     ( Arbitrary (..),
     --         Gen,
@@ -23,9 +22,7 @@ module QuickCheck where
     --         withMaxSuccess,
     --         (==>),
     --     )
-
-    import Solver 
-
+import Solver 
     {- How can we generate an arbitrary board? 
     instance (Ord a, Arbitrary a) => Arbitrary Board where
     arbitrary :: Gen Board
@@ -46,33 +43,44 @@ module QuickCheck where
     shrink (N _ E _ E) = [E]
     shrink (N _ l e r) = [(N 1 E e E), l, r] ++ shrink l ++ shrink r -} 
     
-    prop_valid_solution :: Board -> Bool 
-    prop_valid_solution board = helper board locations 
-        where 
-            helper board [] = True 
-            helper board ((row, col) : t) = 
-                let rows = [(row, c) | c <- [0..width - 1]] in 
-                let cols = [(r, col) | r <- [0..height - 1]] in
-                let boxes = getBox (row, col) in
-                let v = Map.lookup (row, col) board in 
-                case v of 
-                    Nothing -> error "Should not happen"
-                    Just val -> 
-                        (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True boxes) && 
-                        (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True rows) &&
-                        (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True cols) && (helper board t)
+prop_valid_solution :: Board -> Bool 
+prop_valid_solution board = helper board locations 
+    where 
+        helper board [] = True 
+        helper board ((row, col) : t) = 
+            let rows = [(row, c) | c <- [0..width - 1]] in 
+            let cols = [(r, col) | r <- [0..height - 1]] in
+            let boxes = getBox (row, col) in
+            let v = Map.lookup (row, col) board in 
+            case v of 
+                Nothing -> error "Should not happen"
+                Just val -> 
+                    (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 boxes) == 1 && 
+                    (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 rows) == 1 &&
+                    (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 cols) == 1 && (helper board t)
 
-    prop_valid_location :: Loc -> Board -> Bool 
-    prop_valid_location (row, col) board = 
-        let rows = [(row, c) | c <- [0..width - 1]] in 
-        let cols = [(r, col) | r <- [0..height - 1]] in
-        let boxes = getBox (row, col) in
-        let v = Map.lookup (row, col) board in 
-        case v of 
-            Nothing -> error "Should not happen"
-            Just val -> 
-                (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True boxes) && 
-                (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True rows) &&
-                (foldr (\loc -> \acc -> acc && Map.lookup loc board /= Just val) True cols)
+prop_valid_location :: Loc -> Board -> Bool 
+prop_valid_location (row, col) board = 
+    let rows = [(row, c) | c <- [0..width - 1]] in 
+    let cols = [(r, col) | r <- [0..height - 1]] in
+    let boxes = getBox (row, col) in
+    let v = Map.lookup (row, col) board in 
+    case v of 
+        Nothing -> error "Should not happen"
+        Just val -> 
+            (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 boxes) == 1 && 
+            (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 rows) == 1 &&
+            (foldr (\loc -> \acc -> acc + (if Map.lookup loc board == Just val then 1 else 0)) 0 cols) == 1 
 
-    
+prop_no_blanks :: Board -> Bool
+prop_no_blanks board =  helper board locations 
+    where 
+        helper board [] = True 
+        helper board ((row, col) : t) = 
+            let rows = [(row, c) | c <- [0..width - 1]] in 
+            let cols = [(r, col) | r <- [0..height - 1]] in
+            let boxes = getBox (row, col) in
+            let v = Map.lookup (row, col) board in 
+            (foldr (\loc -> \acc -> acc && (Map.lookup loc board /= Just Zero)) True boxes) && 
+            (foldr (\loc -> \acc -> acc && (Map.lookup loc board /= Just Zero)) True rows) &&
+            (foldr (\loc -> \acc -> acc && (Map.lookup loc board /= Just Zero)) True cols) && (helper board t)
