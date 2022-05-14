231629fb0023e6c012f85ecc951727711302f4c7
vyomajani <vyomajani@gmail.com>
2022-05-14 13:00:59 -0400

    Removing unnecessary imports

--- final-proj/src/QuickCheck.hs
+++ final-proj/src/QuickCheck.hs
@@ -3,45 +3,8 @@ import Control.Monad (liftM2, liftM3)
 import Data.List (nub)
 import Data.Map (Map)
 import qualified Data.Map as Map
-    -- import Test.QuickCheck
-    --     ( Arbitrary (..),
-    --         Gen,
-    --         OrderedList (..),
-    --         Property,
-    --         Testable (..),
-    --         choose,
-    --         classify,
-    --         elements,
-    --         forAll,
-    --         frequency,
-    --         label,
-    --         oneof,
-    --         quickCheck,
-    --         sample,
-    --         sized,
-    --         withMaxSuccess,
-    --         (==>),
-    --     )
-import Solver 
-    {- How can we generate an arbitrary board? 
-    instance (Ord a, Arbitrary a) => Arbitrary Board where
-    arbitrary :: Gen Board
-    arbitrary = sized gen where 
-        gen :: Int -> Gen Board
-        gen n = 
-            frequency 
-            [
-                (1, return E), 
-                (n, do 
-                    e <- arbitrary 
-                    new <- gen (n `div` 2)
-                    return $ insert e new) 
-            ]
 
-    shrink :: AVL a -> [AVL a]
-    shrink E = [] -- can't do [E] bc infinite loop 
-    shrink (N _ E _ E) = [E]
-    shrink (N _ l e r) = [(N 1 E e E), l, r] ++ shrink l ++ shrink r -} 
+import Solver 
     
 prop_valid_solution :: Board -> Bool 
 prop_valid_solution board = helper board locations 
